ba60b499132db9db6c92c229569b025849fd3692
julian <jurianu1214@gmail.com>
2022-05-04 09:03:48 +0200

    bichomon

---
+++ scripts.js
@@ -0,0 +1,28 @@
+console.log(document.title);
+
+
+
+
+let generacion = document.querySelector("#gen-1").innerHTML = "Generasion 1 Pokimon"
+
+let color = document.querySelector(".infocard-list")
+
+color.style = "background-color: " + "brown";
+
+let urlPagina = window.location;
+console.log(urlPagina)
+
+console.log(document.domain);
+
+let nodos = document.querySelectorAll("img")
+
+console.log(nodos)
+
+for(i = 0; i <= nodos.length; i++){
+    nodos[i].src = "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+}
+    
+let flying = document.querySelectorAll(".itype flying")
+console.log(flying);
+
+
